80857c31c53b66b4b5c6d4582dabdc95bb516c0a
Innar Made <innar.made@protonmail.com>
2022-10-09 19:22:38 +0100

    Draw hello world style Pong table

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # rust-pong
 Simple 2D retro Pong game written in Rust language using macroquad library
+
+## Local development setup
+```sh
+git clone git@github.com:imade/rust-pong.git && cd rust-pong && cargo run --release
+```
+
+## Troubleshooting
+
+### Linking with cc failed
+
+On Linux you might see the following error when you're trying to `cargo run` this project:
+```sh
+error: linking with `cc` failed: exit status: 1
+```
+
+To fix this issue you need to install additional OS libraries as mentioned in [macroquad readme document](https://github.com/not-fl3/macroquad#linux).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+// Window constants
+const WINDOW_HEIGHT: f32 = 600.;
+const WINDOW_WIDTH: f32 = 800.;
+const WINDOW_HEIGHT_HALF: f32 = WINDOW_HEIGHT / 2.;
+const WINDOW_WIDTH_HALF: f32 = WINDOW_WIDTH / 2.;
+
+// Player rectangle constants
+const RECT_PADDING: f32 = 5.;
+const RECT_HEIGHT: f32 = 80.;
+const RECT_WIDTH: f32 = 20.;
+const RECT_HEIGHT_HALF: f32 = RECT_HEIGHT / 2.;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Rust Pong".to_string(),
+        window_height: WINDOW_HEIGHT as i32,
+        window_width: WINDOW_WIDTH as i32,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(GREEN);
+
+        draw_line(
+            WINDOW_WIDTH_HALF,
+            0.,
+            WINDOW_WIDTH_HALF,
+            WINDOW_HEIGHT,
+            10.0,
+            WHITE,
+        );
+
+        draw_rectangle(
+            RECT_PADDING,
+            WINDOW_HEIGHT_HALF - RECT_HEIGHT_HALF,
+            RECT_WIDTH,
+            RECT_HEIGHT,
+            WHITE,
+        );
+
+        draw_rectangle(
+            WINDOW_WIDTH - RECT_WIDTH - RECT_PADDING,
+            WINDOW_HEIGHT_HALF - RECT_HEIGHT_HALF,
+            RECT_WIDTH,
+            RECT_HEIGHT,
+            WHITE,
+        );
+
+        next_frame().await
+    }
 }
